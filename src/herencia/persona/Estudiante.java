e2e68bb8d3c6fdf314959566178633c62108d34e
korozcos2 <korozcos2@miumg.edu.gt>
2026-08-04 16:44:47 -0600

    feat

--- src/herencia/persona/Estudiante.java
+++ src/herencia/persona/Estudiante.java
@@ -1,5 +1,37 @@
 package herencia.persona;
 
-public class Estudiante {
+/*
+ * ------------------------------------------------------------
+ * Clase Estudiante
+ * ------------------------------------------------------------
+ *
+ * Explicación:
+ * - Estudiante hereda de Persona utilizando extends.
+ * - Se agrega el atributo carnet, que identifica al estudiante.
+ * - Se utiliza super(nombre, edad) para inicializar los atributos
+ *   heredados de la clase Persona.
+ * - Se sobrescribe el método mostrar() utilizando @Override para
+ *   mostrar la información heredada y el carnet.
+ *
+ * Casos de prueba:
+ * 1. Crear un Estudiante.
+ * 2. Verificar que herede nombre y edad.
+ * 3. Verificar que muestre correctamente el carnet.
+ */
+
+public class Estudiante extends Persona {
+
+    private String carnet;
+
+    public Estudiante(String nombre, int edad, String carnet) {
+        super(nombre, edad);
+        this.carnet = carnet;
+    }
+
+    @Override
+    public void mostrar() {
+        super.mostrar();
+        System.out.println("Carnet: " + carnet);
+    }
 
 }
